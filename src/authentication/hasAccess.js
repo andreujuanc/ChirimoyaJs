0fbecd733c5592228fcda0954184a3006ca79303
Juan C. Andreu <andreujuanc@gmail.com>
2017-01-17 21:56:52 +0100

    fixed access processing

--- src/authentication/hasAccess.js
+++ src/authentication/hasAccess.js
@@ -1,21 +1,37 @@
 import getPageComponent from '../core/getPageComponent';
 import chirimoya from '../chirimoya';
 
-var processAccess = function(page, access){
+var _processAccess = function(page, access, isLoggedIn){
+    if(page.access === 'all') return true;
     if(access === false) return false;
-    var result =  page.access === 'all' || window.chirimoya.isLoggedIn() ;
-    return result;
+    if(isLoggedIn === false) return false;     
+    return access;
+};
+
+var processAccess = function(page, access){
+    var loggedInResult = window.chirimoya.isLoggedIn();
+    if(loggedInResult.then && typeof loggedInResult.then ==='function'){
+        return loggedInResult.then(function(isLoggedIn){
+            return _processAccess(page, access, isLoggedIn)
+        });
+    } 
+    else{
+        return _processAccess(page, access, loggedInResult);
+    }   
 };
 
 var hasAccess = function (View, moduleId) {
     return new Promise(function (resolve, reject) {
         try {
             var page = getPageComponent(View);
+            // if(page.access === 'all'){
+            //     return resolve(true);
+            // }
             if (chirimoya.onBeforeHasAccess && typeof chirimoya.onBeforeHasAccess === 'function') {
                 var result = chirimoya.onBeforeHasAccess(View, moduleId);
                 if(result.then && typeof result.then ==='function'){
-                    return result.then(function(cancelAccess){
-                        return resolve(processAccess(page, cancelAccess));
+                    return result.then(function(accessResult){
+                        return resolve(processAccess(page, accessResult));
                     });
                 } 
                 else{
